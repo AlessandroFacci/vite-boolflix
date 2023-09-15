9c356542ef0f51889483a5235d3c0814c6c39751
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-15 15:36:07 +0200

    AppHeader: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
 export default {
   data() {
     return {};
   },
+  components: { AppHeader, AppMain },
 };
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
 
 <style lang="scss">
 @use "./assets/scss/style.scss" as *;
