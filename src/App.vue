94fc94356c811414dd0ff6e44ad3864d8b815dc6
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-15 16:39:42 +0200

    App: add axios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script>
+import axios from "axios";
+
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      movies: [],
+    };
   },
+
   components: { AppHeader, AppMain },
+
+  methods: {
+    fetchMovies() {
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            query: "anelli",
+            api_key: "02bab489fc90a269b3344630a53a52ed",
+          },
+        })
+        .then((response) => {
+          console.log(response.data.results);
+          this.movies = response.data.results;
+          console.log(this.movies);
+        });
+    },
+  },
+
+  created() {
+    this.fetchMovies();
+  },
 };
 </script>
 
